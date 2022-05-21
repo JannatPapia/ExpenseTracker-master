c0d6881e0099efb5400a4632ee0088a150d826a6
Jannatpapia <papiamcode@gmail.com>
2022-05-21 23:47:40 +0600

    Group Transactions By Month

--- TransactionListViewModel.swift
+++ TransactionListViewModel.swift
@@ -46,4 +46,12 @@ final class  TransactionListViewModel: ObservableObject {
             }
             .store(in: &cancellables)
     }
+    
+    func groupTransactionsByMonth() -> TransactionGroup {
+        guard !transactions.isEmpty else {return [:] }
+        
+        let groupedTransactions = TransactionGroup(grouping: transactions) { $0.month }
+        
+        return groupedTransactions
+    }
 }
